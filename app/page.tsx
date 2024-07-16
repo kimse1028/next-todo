397f4270309fc8290854bf61a48b2dc587e878c8
ksh <zzedai@gmail.com>
2024-07-16 10:47:32 +0900

    코드정리 조금

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useAuth } from '../contexts/AuthContext';
-import { signInWithGoogle } from '../lib/auth';
 import Link from 'next/link';
 import { Button } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
@@ -14,16 +13,6 @@ export default function Home() {
   useEffect(() => {
     router.push('/login');
   }, [router]);
-
-  const handleGoogleLogin = async () => {
-    try {
-      await signInWithGoogle();
-      router.push('/todos');
-    } catch (error) {
-      console.error('Google login error:', error);
-      alert('구글 로그인에 실패했습니다. 다시 시도해 주세요.');
-    }
-  };
 
   return (
     <div>
